2ef063e66f6e143ad1ff8692855db3671c586f84
kangju <kangju0815@yahoo.co.jp>
2015-05-09 13:29:18 +0900

    create staff member seed data

---
+++ db/seeds/development/staff_members.rb
@@ -0,0 +1,8 @@
+StaffMember.create!(email: 'otinpo@sample.com',
+   family_name: '上島',
+   given_name: '竜兵',
+   family_name_kana: 'ウエシマ',
+   given_name_kana: 'リュウヘイ',
+   password: 'ueda',
+   start_date: Date.today,
+   )
